8cab1b94aa8502a14f975706634fbc2ae0309143
Solexgreat <solomonGodkid@gmail.com>
2023-02-10 01:45:45 +0100

    Minimum Operation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,29 +2,19 @@
 """Return the minimum Operation used"""
 
 
-def minOperations(n: int)-> int:
+def minOperations(n):
     """function calculate the minimum operation to get to (n)"""
-    H: int = 1
-    i: int = 0
-    result = False
-    if type(n) != int or n < 0 or n == 1:
+    H = 1
+    i = 0
+
+    if isinstance(n, int) or n < 2:
         return (0)
-    while H <= n:
-        if H != 1 and n % H == 0 and H < n and H != n:
-            H += 1
-            result = True
-            break
-        H += 1
-        result = False
-    if result == False:
-        return (0)    
-    if result:
-        H = 1
-        while H < n:
-            if H == 1 or n % H == 0:
-                H *= 2
-                i += 2
-            else:
-                H += H/2
-                i += 1
+  
+    while H < n:
+        if H == 1 or n % H == 0:
+            H *= 2
+            i += 2
+        else:
+            H += H/2
+            i += 1
         return(i)
