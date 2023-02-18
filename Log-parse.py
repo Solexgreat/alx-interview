da091d169d801708d02b19eaede20d9c5bb6879a
Solexgreat <solomonGodkid@gmail.com>
2023-02-19 00:19:22 +0100

    Log parsee

--- Log-parse.py
+++ Log-parse.py
@@ -5,7 +5,7 @@ import re
 """line = '<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>' """
 
 total_size = 0
-status_code = {'200':0, '301': 0, '400': 0, '401': 0, '403': 0,
+status_code = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
                 '404': 0, '405': 0, '500': 0 }
 index = 0
 
@@ -13,8 +13,7 @@ for line in sys.stdin:
     index += 1
     if KeyboardInterrupt and index == 10:
         index = 0
-        for i, j in status_code.items():
-            if j > 0:
+        for i,j in status_code.items():
                 print(f"{i}: {j}")
     if re.match(r'^\S+ - \[.+\] "GET /projects/260 HTTP/1.1" \d{3} \d+$', line):
         match = re.search(r'"GET /projects/260 HTTP/1.1" (\d+) (\d+$)', line)
