0f088af03afd7ef5a251fa637d3aff65ef34feda
Solexgreat <solomonGodkid@gmail.com>
2023-02-19 00:36:18 +0100

    Log parsee

--- Log-parse.py
+++ Log-parse.py
@@ -8,22 +8,27 @@ total_size = 0
 status_code = {'200':0, '301': 0, '400': 0, '401': 0, '403': 0,
                 '404': 0, '405': 0, '500': 0 }
 index = 0
-
-for line in sys.stdin:
-    index += 1
-    if KeyboardInterrupt and index == 10:
-        index = 0
-        print(f"File size: {total_size}")
-        for i, j in status_code.items():
-            if j > 0:
-                print(f"{i}: {j}")
-    if re.match(r'^\S+ - \[.+\] "GET /projects/260 HTTP/1.1" \d{3} \d+$', line):
-        match = re.search(r'"GET /projects/260 HTTP/1.1" (\d+) (\d+$)', line)
-        if match:
-            code = match.group(1)
-            if code in status_code:
-                status_code[code] += 1
-        file = match.group(2)
-        total_size += int(file)
-    else:
-        continue    
+try:
+    for line in sys.stdin:
+        index += 1
+        if  index == 10:
+            index = 0
+            print(f"File size: {total_size}")
+            for i, j in status_code.items():
+                if j > 0:
+                    print(f"{i}: {j}")
+        if re.match(r'^\S+ - \[.+\] "GET /projects/260 HTTP/1.1" \d{3} \d+$', line):
+            match = re.search(r'"GET /projects/260 HTTP/1.1" (\d+) (\d+$)', line)
+            if match:
+                code = match.group(1)
+                if code in status_code:
+                    status_code[code] += 1
+            file = match.group(2)
+            total_size += int(file)
+        else:
+            continue
+except KeyboardInterrupt:
+    for i, j in status_code.items():
+                if j > 0:
+                    print(f"{i}: {j}")
+    raise                
